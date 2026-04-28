45435a19858c2ec17500a1ecd1f0570c23ce4882
albert <albert.maharjan94@gmail.com>
2026-04-28 10:22:30 +0545

    init

--- src/types/product.type.ts
+++ src/types/product.type.ts
@@ -5,11 +5,27 @@
 // category as optional string 
 // z.string().optional() // optional
 // z.default("Unknown Product") // default value if not provided
+import { z } from "zod";
 
+export const ProductSchema = z.object({
+    id: z.string(),
+    name: z.string()
+        .min(1, "Name is required")
+        .default("Unknown Product"),
+    price: z.number()
+        .min(0, "Price must be a positive number"),
+    category: z.string().optional()
+});
+
+export type Product = z.infer<typeof ProductSchema>;
 // make a dto from this schema for create
 // on create, id is not required
 
 // make a dto from this schema for update
 // on update, all fields are optional 
+export const UpdateProductDTO = 
+    ProductSchema
+    .partial() // make all optional
+    .omit({ id: true });
 
 // apply model, dto, and controller for product similar to person
