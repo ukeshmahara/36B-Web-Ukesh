e490f79be9cf52b1fb4daf1054e433860d436b07
albert <albert.maharjan94@gmail.com>
2026-06-01 09:13:17 +0545

    may 29

---
+++ src/middlewares/authorized.middleware.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";  
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../config/constant";
+import { HttpException } from "../exceptions/http-exception";
+import { IUser } from "../models/user.model";
+import { ApiResponseHelper } from "../utils/api-response";
+import { UserMongoRepository } from "../repositories/user.repository";
+
+const userRepository = new UserMongoRepository();
+// user tag implementation
+declare global {
+    namespace Express {
+        interface Request {
+            user?: Record<string, any> | IUser; // Add user property, Request interface
+        }
+    }
+}// for user detail now can be accessed in req.user
+export const authorizedMiddleware = 
+    async (req: Request, res: Response, next: NextFunction) => {
+        try{
+            const authHeader = req.headers.authorization;
+            if(!authHeader || !authHeader.startsWith("Bearer "))
+                throw new HttpException(401, "Authorization header missing or malformed");
+            const token = authHeader.split(" ")[1]; // Bearer-> 0, <token>-> 1
+            if(!token)
+                throw new HttpException(401, "Token missing");
+            const decoded = jwt.verify(token, SECRET_KEY) as Record<string, any>;
+            if(!decoded || !decoded.id)
+                throw new HttpException(401, "Invalid token");
+            const user = await userRepository.findById(decoded.id);
+            if(!user)
+                throw new HttpException(401, "User not found");
+            req.user = user; // attach user to request object for downstream use
+            return next(); // entry ahead
+        }catch(e: Error | unknown | any){
+            return ApiResponseHelper.error(
+                res, 
+                e?.message || "Unauthorized", 
+                e.status || 401
+            );
+        }
+}
